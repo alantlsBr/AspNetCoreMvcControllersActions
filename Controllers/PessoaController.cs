207acedff3f63e32c1e5f770d915f4f5599be7e2
Alan Teles de Oliveira <alantls@hotmail.com.br>
2019-08-06 21:45:57 -0400

    formulario com classe e chamada dos metodos

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreMvcControllersActions.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AspNetCoreMvcControllersActions.Controllers
 {
-    public class OutroController : Controller
+    public class PessoaController : Controller
     {
-        public ViewResult Index()
+        public IActionResult Formulario()
         {
-            return View("Resultado", "Outro controller");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Formulario(Pessoa pessoa)
+        {
+            return View("Pessoa", pessoa);
         }
     }
 }
